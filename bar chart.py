a24e94e8eed2953c652543a285d5f36e23cde56a
manulasya09-tech <manulasya09@gmail.com>
2025-09-26 11:11:13 +0530

    Add files via upload

---
+++ bar chart.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+#2.Bar Chart
+categories=['A','B','C','D']
+values=[23,45,56,78]
+plt.bar(categories,values,color='green')
+plt.title("Bar chart example:")
+plt.show()
